a50c7194805ef96426ee68d734be3608a4cd83ff
MiVladie <mivladie@github.com>
2019-11-11 20:32:16 +0000

    Updates

---
+++ precache-manifest.151324a3d6949705d1ecf5db88718243.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12570c57c0e32eab9e27917fb48aeb5e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e63efc6e5983e688e03",
+    "url": "/portfolio/static/css/main.f6043e22.chunk.css"
+  },
+  {
+    "revision": "9860a5769017e2966d04",
+    "url": "/portfolio/static/js/2.78dae8c9.chunk.js"
+  },
+  {
+    "revision": "2e63efc6e5983e688e03",
+    "url": "/portfolio/static/js/main.4f97f37e.chunk.js"
+  },
+  {
+    "revision": "8aa216a2b35680545440",
+    "url": "/portfolio/static/js/runtime~main.75d78480.js"
+  },
+  {
+    "revision": "766027989a245b05e4328edaece3179e",
+    "url": "/portfolio/static/media/burgerbuilder.76602798.png"
+  },
+  {
+    "revision": "4242477966f34d5faca18b1ccfcbf43c",
+    "url": "/portfolio/static/media/email.42424779.png"
+  },
+  {
+    "revision": "c38514406a62816870fd98ef5b41d9cc",
+    "url": "/portfolio/static/media/journeyplanner.c3851440.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b669ee99ce050ccd59f77f0f0ebaeb9e.js"
+  "/portfolio/precache-manifest.151324a3d6949705d1ecf5db88718243.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
